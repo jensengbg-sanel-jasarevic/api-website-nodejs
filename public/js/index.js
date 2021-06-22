29caaa6e8602da12781c40f592db88831f717c55
jensengbg-sanel-jasarevic <55239936+jensengbg-sanel-jasarevic@users.noreply.github.com>
2021-06-22 02:35:47 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,78 +1,99 @@
 // Local API server URL: http://localhost:7000
-// Cloud API server URL: https://airbean-api-server.herokuapp.com
+// Cloud API server URL: https://eshop-node.herokuapp.com
 
 const API_URL = 'https://eshop-node.herokuapp.com'
 
-const showBasket = document.querySelector('.show-basket');
+const addButton = document.querySelector('.buttonSubmit');
+const productShop = document.querySelector('.products-shop');
 
-fetch(API_URL + '/basket', { method: 'GET' })
+// Load all products when opening site (GET)
+fetch(API_URL + '/products', { method: 'GET' })
   .then(response => {
     return response.json();
   })
   .then(data => {
-    basketProducts(data);
+    showProducts(data);
   });
 
-const basketProducts = dataBasket => {
-  for (let i = 0; i < dataBasket.length; i++) {
-    let divTag = document.createElement('div');
+const checkBasket = () => {
+  fetch(API_URL + '/basket', { method: 'GET' })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      data.forEach(data => {
+        // Get the name of product through database then assign it to the variable 
+        let productId = data.productName;
+        // Find id of clicked button (id assigned with name of product in showProducts when button element is created) 
+        let checkButton = document.getElementById(productId);
+        checkButton.classList.remove('buttonSubmit');
+        checkButton.classList.add('buttonDisable');
+        checkButton.disabled = true;
+        checkButton.innerHTML = 'Added to basket';
+      });
+    });
+};
+
+// Get all products, after do this function with the data
+// Create elements to the HTML DOM
+const showProducts = dataProducts => {
+  for (let i = 0; i < dataProducts.length; i++) {
     let productTag = document.createElement('p');
     let priceTag = document.createElement('p');
     let imgTag = document.createElement('img');
     let buttonTag = document.createElement('button');
 
-    buttonTag.innerHTML = 'Delete from basket';
+    productTag.setAttribute('class', 'product');
+    priceTag.setAttribute('class', 'price');
+    imgTag.setAttribute('class', 'image');
+    buttonTag.setAttribute('class', 'buttonSubmit');
+    buttonTag.setAttribute('type', 'submit');
+    buttonTag.setAttribute('id', `${dataProducts[i].productName}`);
+    buttonTag.innerHTML = 'Add product to basket';
 
-    divTag.setAttribute('id', `product${dataBasket[i].productName}`);
-    divTag.setAttribute('class', 'div-box');
-    productTag.setAttribute('id', dataBasket[i].productName);
-    priceTag.setAttribute('id', dataBasket[i].productPrice);
-    imgTag.setAttribute('id', dataBasket[i].productImage);
-    buttonTag.setAttribute('class', 'buttonDelete');
+    imgTag.setAttribute('width', '200');
+    imgTag.setAttribute('height', '200');
 
-    imgTag.setAttribute('width', '250');
-    imgTag.setAttribute('height', '250');
-    buttonTag.style.height = '2.5rem';
-    buttonTag.style.width = '15%';
+    productTag.innerHTML = dataProducts[i].productName;
+    priceTag.innerHTML = dataProducts[i].productPrice;
+    imgTag.src = dataProducts[i].productImage;
 
-    productTag.innerHTML = dataBasket[i].productName;
-    priceTag.innerHTML = dataBasket[i].productPrice;
-    imgTag.src = dataBasket[i].productImage;
+    productShop.append(imgTag);
+    productShop.append(productTag);
+    productShop.append(priceTag);
+    productShop.append(buttonTag);
 
-    showBasket.append(divTag);
-    divTag.appendChild(imgTag);
-    divTag.appendChild(productTag);
-    divTag.appendChild(priceTag);
-    divTag.appendChild(buttonTag);
-
-    buttonTag.addEventListener('click', e => {   
-      eraseElements(
-        dataBasket[i].productName,
-        dataBasket[i].productPrice,
-      );
+    buttonTag.addEventListener('click', e => {
+      addBasket(dataProducts);
     });
-    
-    const eraseElements = (nameParam, priceParam) => {
-      let productTagErase = document.getElementById(`${nameParam}`).innerHTML;
-      let priceTagErase = document.getElementById(`${priceParam}`).innerHTML;
-      let productBoxErase = document.getElementById(`product${nameParam}`);
 
-      const deleteUrl =
+    // Add product to basket (POST)
+    const addBasket = () => {
+      let name = document.querySelectorAll('.product')[i].innerHTML;
+      let price = document.querySelectorAll('.price')[i].innerHTML;
+
+      const postUrl =
         API_URL +
-        '/basket/deleteproducts' +
+        '/basket/addproducts' +
         '?name=' +
-        productTagErase +
+        name +
         '&price=' +
-        priceTagErase;
+        price;
 
-      fetch(deleteUrl, { method: 'DELETE' })
+      fetch(postUrl, { method: 'POST' })
         .then(response => {
           return response.json();
         })
         .then(data => {
-          alert(data.message);
+          console.log(data);
+          checkBasket(data);
         });
-      productBoxErase.remove();
     };
   }
 };
+
+// Grab the browser window object, when it has fully loaded (all HTML-tags loaded in), do following
+// Check whether basket is empty or already containing products
+window.addEventListener('load', () => {
+  checkBasket();
+});
